8b3c99b242a631481bdb7c109fedf133d8deffe3
web0Impulse <smoug2001@mail.ru>
2025-11-13 19:27:06 +0400

    Add route UserData1

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from controllers.device_state import DeviceState
 from controllers.signup import SignUp
 from app_data.definitions import mysql_connection
 from controllers.user_data import UserData
+from controllers.user_data_1 import UserData1
 
 def InitRoutes(api):
     additional_params = {
@@ -17,4 +18,6 @@ def InitRoutes(api):
     api.add_resource(SignUp, '/api/v1/signup',
                      resource_class_kwargs=additional_params)
     api.add_resource(UserData, '/api/v1/user/<int:user_id>',
+                     resource_class_kwargs=additional_params)
+    api.add_resource(UserData1, '/api/v1/me',
                      resource_class_kwargs=additional_params)
