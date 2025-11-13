85a8b2b321831583657425213936bb091e65cc01
web0Impulse <smoug2001@mail.ru>
2025-11-13 17:57:31 +0400

    1) Add html flag; 2) Return dependc from err status

--- classes/EmailCodeSender.py
+++ classes/EmailCodeSender.py
@@ -11,20 +11,24 @@ class EmailCodeSender(object):
         self._source_email = source_email
         self._password = password
         
-    def send(self, addr: str, subj: str, message: str) -> bool:
+    def send(self, addr: str, subj: str, message: str, isHtml:bool = False) -> bool:
         try:
             self._smtp_obj = smtplib.SMTP_SSL(self._smtp_serv, self._port)
             self._smtp_obj.login(self._source_email, self._password)
             msg = MIMEMultipart()
-            msg.attach(MIMEText(message, 'plain'))
+            if isHtml:
+                msg.attach(MIMEText(message, 'html'))
+            else:
+                msg.attach(MIMEText(message, 'plain'))
             msg['From'] = self._source_email
             msg['To'] = addr
             msg['Subject'] = subj
             errors = self._smtp_obj.send_message(msg)
             self._smtp_obj.quit()
+            return True
         except Exception as e:
             self._smtp_obj.quit()
-        return False
+            return False
     
     def send_async(self, addr: str, subj: str, message: str) -> bool:
         Thread(target=self.send, args=(addr, subj, message)).start()
