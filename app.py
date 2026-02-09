18ff0a2d3c49d33b920be595ee8949843d35b71b
= <=>
2026-02-09 14:31:07 +0400

    Добавлены конфиги для загрузки изображений

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, session
 from flask_session import Session
 from flask_restful import Api
 from routes import InitRoutes
-from os import environ
+from os import environ, path, sep
 from app_data.definitions import server_port
 
 
@@ -12,6 +12,10 @@ api = Api(app)
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
 Session(app)
+
+app.config['APP_PATH'] = path.dirname(__file__)
+app.config['UPLOAD_FOLDER'] = app.config['APP_PATH'] + sep + 'dbimages'
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 100
 
 InitRoutes(api)
 
